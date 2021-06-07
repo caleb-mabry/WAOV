f552d410636018d2224b5e47cc06dc1fd941ca8b
Caleb Mabry <caleb.mabry.15@cnu.edu>
2021-06-07 01:06:02 -0400

    Added typescript through Vue CLI, I think Im going to suffer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-    <div id="nav">
-      <router-link to="/">Server Select</router-link>
-  </div>
-  <router-view />
-
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: "App",
-};
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
@@ -22,5 +24,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
